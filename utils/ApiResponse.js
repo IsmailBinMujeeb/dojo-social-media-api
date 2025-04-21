e554b55f2ec7ce047160b386f7c96a96048dd216
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-04-21 17:32:17 +0530

    update ApiResponse class

--- utils/ApiResponse.js
+++ utils/ApiResponse.js
@@ -1,17 +1,28 @@
 export default class ApiResponse {
 
-    constructor(statusCode = 200, message = "ok", data = [], errors=[]) {
+    constructor(statusCode = 200, message = "ok", data = [], errors = []) {
 
         this.statusCode = statusCode;
         this.message = message;
-        this.data = this.getData(data);
+        this.data = data;
         this.success = statusCode < 400;
         this.errors = errors
     }
 
-    getData(data) {
-        if (Array.isArray(data) && !data.length) return [];
+    static UserResponse(statusCode = 200, message = "ok", data = [], errors = []) {
 
-        return { _id: data._id, firstName: data.firstName, lastName: data.lastName, userhandle: data.userhandle, email: data.email, proefilePicture: data.profilePicture }
+        const userData = { _id: data._id, firstName: data.firstName, lastName: data.lastName, userhandle: data.userhandle, email: data.email, profilePicture: data.profilePicture }
+        return new ApiResponse(200, "ok", userData);
+    }
+
+    static PostResponse(statusCode = 200, message = "ok", data = [], errors = []) {
+
+        let postData = [];
+
+        if (!Array.isArray(data))
+            postData = { _id: data._id, title: data.title, content: data.content, createdAt: data.createdAt, updatedAt: data.updatedAt, userId: data.userId }
+        else
+            postData = data;
+        return new ApiResponse(statusCode, message, postData, errors);
     }
 }
